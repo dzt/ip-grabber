32a24e25cfea6f7280e17bfc9e408da78e97d3f0
Peter Kardashian <thepcmrtim@gmail.com>
2014-02-21 20:35:04 -0800

    Simple IP Grabber I made in PHP
    LOOK AT README.md file for tutorial.
    
    Don't be stupid and use this to threaten people i made this for
    protection and this can be used for you want you can jack up and fiddle
    around with the source code i am not 0.01% responsable if you perform a
    stupid act.
    
    - Peter aka dzt aka Virtual God

---
+++ ip-grabber.php
@@ -0,0 +1,23 @@
+<?php
+ 
+//IP Grabber
+ 
+//Variables
+ 
+$protocol = $_SERVER['SERVER_PROTOCOL'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$port = $_SERVER['REMOTE_PORT'];
+$agent = $_SERVER['HTTP_USER_AGENT'];
+$ref = $_SERVER['HTTP_REFERER'];
+$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+ 
+//Print IP, Hostname, Port Number, User Agent and Referer To Log.TXT
+ 
+$fh = fopen('log.txt', 'a');
+fwrite($fh, 'IP Address: '."".$ip ."\n");
+fwrite($fh, 'Hostname: '."".$hostname ."\n");
+fwrite($fh, 'Port Number: '."".$port ."\n");
+fwrite($fh, 'User Agent: '."".$agent ."\n");
+fwrite($fh, 'HTTP Referer: '."".$ref ."\n\n");
+fclose($fh);
+?>
